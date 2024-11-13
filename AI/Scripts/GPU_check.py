b1df50f6478cbb12c344b3ae22a59bf513faac5e
Jakub Kiryła <jakub.kiryla@gmail.com>
2024-11-13 22:52:39 +0100

    added gpu computing
    Separate checker script

---
+++ AI/Scripts/GPU_check.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+
+physical_devices = tf.config.list_physical_devices('GPU')
+
+if physical_devices:
+    print("Znaleziono GPU:")
+    for device in physical_devices:
+        print(device)
+    
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+else:
+    print("Nie znaleziono GPU. Używany jest CPU.")
